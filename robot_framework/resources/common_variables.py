591b0138f84781d311612e8ec7831c65cb5172d9
kissbalazs92 <kiss.balazs@test-it.hu>
2023-04-12 21:56:44 +0200

    test

--- robot_framework/resources/common_variables.py
+++ robot_framework/resources/common_variables.py
@@ -1,7 +1,7 @@
 import os
 
 BROWSER = os.environ.get("BROWSER", "Firefox")
-BASE_URL = "http://web:80" #lokálban futtatáshoz át kell írni: localhost:8080
+BASE_URL = "https://www.google.com/" #"http://web:80" #lokálban futtatáshoz át kell írni: localhost:8080
 USERNAME = "user"
 PASSWORD = "user123"
 ADMIN = "admin"
